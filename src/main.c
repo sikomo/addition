149f0971ebba0f10b7aa045943d122007eef0eeb
gittest35 <gittest35@gmail.com>
2023-03-10 14:18:34 +0100

    Ajout d'un commentaire dans main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 int main(int argc, char const *argv[]) {
   int a = 10, b = 5;
 
-  printf("%d + %d = %d \n",a, b, somme(a, b) );
+  printf("%d + %d = %d \n",a, b, somme(a, b) ); // ajout d'un commentaire <== ICI
   printf("%d - %d = %d \n",a, b, soustraction(a, b));
   return 0;
 }
